97b17e885392b8508012a62b6effeb58c6c4a7bd
Oleg Chursin <olegchursin@gmail.com>
2017-11-30 18:50:53 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   contacts["Freddy Mercury"].delete_if do |key, value|
     value == "strawberry"
-
+end
 
   #remember to return your newly altered contacts hash!
   contacts
